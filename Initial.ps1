75e3b6c14ba82e7cfc0b5a74e009868148e29af4
MFunction <MFunction96@outlook.com>
2019-01-31 10:19:56 +0800

    Fix Project Initial

--- Initial.ps1
+++ Initial.ps1
@@ -1,5 +1,3 @@
 $parent = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$path = $parent + "\MFSystemInterface\ref\MFStandardUtil"
+$path = $parent + "\ref\MFStandardUtil"
 git clone -q --branch=master https://github.com/MFunction96/MFStandardUtil.git $path
-nuget restore
-dotnet restore
